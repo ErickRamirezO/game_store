293cda462529e04625c0704603960682c2cd59aa
ErickRamirezO <epramirez2@espe.edu.ec>
2025-02-26 18:01:00 -0500

    imoprt images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import darvemyImage from './img/darvemy.jpg';
+import darvemyImage from './assets/images/darvemy.jpg';
 import { 
   ChevronLeft, 
   ChevronRight, 
